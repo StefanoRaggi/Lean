827de8761a0c9ebbcd71bbd0fc13929812f86ceb
Stefano Raggi <stefano.raggi67@gmail.com>
2018-02-22 16:11:27 +0100

    Add regression tests for Forex internal feeds

---
+++ Algorithm.CSharp/ForexDailyDataPointsRegressionAlgorithm.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using QuantConnect.Data;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    /// <summary>
+    /// This algorithm is a test case to count expected data points with forex symbols at daily resolution.
+    /// </summary>
+    public class ForexDailyDataPointsRegressionAlgorithm : QCAlgorithm
+    {
+        private int _count;
+
+        public override void Initialize()
+        {
+            SetStartDate(2013, 10, 7);  //Set Start Date
+            SetEndDate(2013, 10, 9);    //Set End Date
+            SetCash(100000);             //Set Strategy Cash
+
+            AddForex("EURGBP", Resolution.Daily);
+        }
+
+        public override void OnData(Slice data)
+        {
+            foreach (var kvp in data)
+            {
+                _count++;
+                Log($"{Time} {kvp.Key.Value} {kvp.Value.Price}");
+            }
+        }
+
+        public override void OnEndOfAlgorithm()
+        {
+            // one point per day
+            const int expectedDataPoints = 3;
+            Log($"Data points: {_count}");
+
+            if (_count != 3)
+            {
+                throw new Exception($"Data point count mismatch: expected: {expectedDataPoints}, actual: {_count}");
+            }
+        }
+    }
+}
---
+++ Algorithm.CSharp/ForexInternalFeedOnDataRegressionAlgorithm.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using QuantConnect.Data;
+
+namespace QuantConnect.Algorithm.CSharp
+{
+    /// <summary>
+    /// This algorithm is a test case for adding forex symbols at a resolution differing from an existing internal feed.
+    /// The second symbol is added in the OnData method.
+    /// </summary>
+    public class ForexInternalFeedOnDataRegressionAlgorithm : QCAlgorithm
+    {
+        private readonly Dictionary<Symbol, int> _dataPointsPerSymbol = new Dictionary<Symbol, int>();
+        private bool _added;
+
+        public override void Initialize()
+        {
+            SetStartDate(2013, 10, 7);
+            SetEndDate(2013, 10, 8);
+            SetCash(100000);
+
+            var eurgbp = AddForex("EURGBP", Resolution.Daily);
+            _dataPointsPerSymbol.Add(eurgbp.Symbol, 0);
+        }
+
+        public override void OnData(Slice data)
+        {
+            if (!_added)
+            {
+                var gbpusd = AddForex("GBPUSD");
+                _dataPointsPerSymbol.Add(gbpusd.Symbol, 0);
+
+                _added = true;
+            }
+
+            foreach (var kvp in data)
+            {
+                var symbol = kvp.Key;
+                _dataPointsPerSymbol[symbol]++;
+
+                Log($"{Time} {symbol.Value} {kvp.Value.Price}");
+            }
+        }
+
+        public override void OnEndOfAlgorithm()
+        {
+            // (1440 minutes/day * 2 days) + 1
+            const int expectedDataPointsPerSymbol = 2881;
+
+            foreach (var kvp in _dataPointsPerSymbol)
+            {
+                var symbol = kvp.Key;
+                var actualDataPoints = _dataPointsPerSymbol[symbol];
+                Log($"Data points for symbol {symbol.Value}: {actualDataPoints}");
+
+                if (actualDataPoints != expectedDataPointsPerSymbol)
+                {
+                    throw new Exception($"Data point count mismatch for symbol {symbol.Value}: expected: {expectedDataPointsPerSymbol}, actual: {actualDataPoints}");
+                }
+            }
+        }
+    }
+}
